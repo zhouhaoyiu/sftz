44d1f43813da9a4d6fa6ef9a57fa01f0c3a93187
Mystic <67226385+zhouhaoyiu@users.noreply.github.com>
2023-03-15 17:19:33 +0800

    some fix

--- src/views/manageInfo.vue
+++ src/views/manageInfo.vue
@@ -3,14 +3,16 @@
     style="width: 100%; height: 100%; display: flex; flex-direction: column"
     class="page"
   >
-    <!-- {{ userInfoArr }} -->
+    <div style="margin-top: 20px">
+      <el-button @click="addUser" type="primary">新建用户</el-button>
+    </div>
     <el-table
       class="userInfoTable"
       align="center"
       border
       stripe
       :data="userInfoArr"
-      style="width: 90%; margin-top: 40px"
+      style="width: 90%; margin-top: 20px"
     >
       <!-- <el-table-column prop="userId" label="ID" width="180"></el-table-column> -->
       <el-table-column
@@ -51,12 +53,6 @@
       ></el-table-column>
       <el-table-column
         align="center"
-        prop="waterNumber"
-        label="用水量"
-        width="100"
-      ></el-table-column>
-      <el-table-column
-        align="center"
         prop="userPhone"
         label="用户电话"
         width="150"
@@ -92,10 +88,134 @@
     </el-table>
     <el-dialog
       class="dialog"
+      title="新建用户"
+      v-model="addUserDialogVisible"
+      width="60%"
+      top="10vh"
+      :before-close="handleClose"
+    >
+      <div class="dialogDiv">
+        <label for="userHh">户号</label>
+        <el-input
+          id="userHh"
+          v-model="addUserForm.userHh"
+          placeholder="户号"
+        ></el-input>
+      </div>
+      <div class="dialogDiv">
+        <label for="userName">用户名</label>
+        <el-input
+          id="userName"
+          v-model="addUserForm.userName"
+          placeholder="用户名"
+        ></el-input>
+      </div>
+      <div class="dialogDiv">
+        <label for="userAddress">地址</label>
+        <el-input
+          id="userAddress"
+          v-model="addUserForm.userAddress"
+          placeholder="地址"
+        ></el-input>
+      </div>
+      <div class="dialogDiv">
+        <label for="currentNumber">本月指数</label>
+        <el-input
+          id="currentNumber"
+          v-model="addUserForm.currentNumber"
+          placeholder="用户电话"
+        ></el-input>
+      </div>
+      <div class="dialogDiv">
+        <label for="lastNumber">上月指数</label>
+        <el-input
+          id="lastNumber"
+          v-model="addUserForm.lastNumber"
+          placeholder="上月指数"
+        ></el-input>
+      </div>
+      <div
+        style="
+          margin-top: 10px;
+          display: flex;
+          flex-direction: column;
+          width: 100%;
+        "
+      >
+        <div
+          style="
+            display: flex;
+            flex-direction: row;
+            width: 100%;
+            margin-bottom: 10px;
+            align-items: center;
+          "
+          v-for="(item, index) in addUserForm.waterClassification"
+          :key="index"
+        >
+          <!-- TODO:改为select -->
+          <div>
+            <label for="waterType">水性{{ index + 1 }}</label>
+            <el-input
+              id="waterType"
+              v-model="item.waterType"
+              placeholder="水性"
+              style="width: 300px; padding-right: 14px"
+            ></el-input>
+          </div>
+          <div>
+            <label for="waterNumber">默认用水量</label>
+            <el-input
+              id="waterNumber"
+              v-model="item.waterNumber"
+              placeholder="默认用水量"
+              style="width: 300px; padding-right: 14px"
+            ></el-input>
+          </div>
+          <el-button
+            type="danger"
+            @click="handleDeleteSubmitWaterClassification(index)"
+            style="margin-left: 10px"
+          >
+            删除
+          </el-button>
+        </div>
+      </div>
+      <el-button
+        type="primary"
+        @click="handleAddSubmitWaterClassification"
+        style="margin-left: 10px"
+      >
+        添加水性
+      </el-button>
+
+      <div class="dialogDiv">
+        <label for="userPhone">用户电话</label>
+        <el-input
+          id="userPhone"
+          v-model="addUserForm.userPhone"
+          placeholder="用户电话"
+        ></el-input>
+      </div>
+      <div class="dialogDiv">
+        <label for="userWx">用户微信</label>
+        <el-input
+          id="userWx"
+          v-model="addUserForm.userWx"
+          placeholder="用户微信"
+        ></el-input>
+      </div>
+      <div class="dialogDiv">
+        <el-button @click="dialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="submitAddUser"> 确 定 </el-button>
+      </div>
+    </el-dialog>
+    <el-dialog
+      class="dialog"
       title="编辑"
       v-model="dialogVisible"
       width="60%"
-      top="10vh"
+      top="8vh"
       :before-close="handleClose"
     >
       <div class="dialogDiv">
@@ -147,14 +267,6 @@
         ></el-input>
       </div>
       <div class="dialogDiv">
-        <label for="waterNumber">用水量</label>
-        <el-input
-          id="waterNumber"
-          v-model="editForm.waterNumber"
-          placeholder="用水量"
-        ></el-input>
-      </div>
-      <div class="dialogDiv">
         <label for="userPhone">用户电话</label>
         <el-input
           id="userPhone"
@@ -181,14 +293,15 @@
           <label style="padding-right: 14px" for="waterType">
             水性{{ index + 1 }}
           </label>
+          <!-- TODO:改为select -->
           <el-input
             id="waterType"
             v-model="item.waterType"
             placeholder="水性"
             style="width: 300px; padding-right: 14px"
           ></el-input>
-          <label style="padding-right: 14px" for="waterNumber"
-            >默认用水量
+          <label style="padding-right: 14px" for="waterNumber">
+            默认用水量
           </label>
           <el-input
             id="waterNumber"
@@ -230,9 +343,22 @@
 <!-- eslint-disable no-undef -->
 <script lang="ts" setup>
 // import { ElMessage } from "element-plus";
+import dayjs from "dayjs";
 import { onMounted, ref, type Ref } from "vue";
 let userInfoArr: Ref<any[]> = ref([]);
 let dialogVisible: Ref<boolean> = ref(false);
+let addUserDialogVisible: Ref<boolean> = ref(false);
+let addUserForm = ref({
+  userHh: "",
+  jfyf: "",
+  userName: "",
+  userAddress: "",
+  currentNumber: "",
+  lastNumber: "",
+  userPhone: "",
+  userWx: "",
+  waterClassification: [] as any[],
+});
 let editForm: Ref<any> = ref({
   userHh: "",
   jfyf: "",
@@ -240,7 +366,6 @@ let editForm: Ref<any> = ref({
   userAddress: "",
   currentNumber: "",
   lastNumber: "",
-  waterNumber: "",
   userPhone: "",
   userWx: "",
   waterClassification: [
@@ -248,8 +373,35 @@ let editForm: Ref<any> = ref({
       waterType: "生活一",
       waterNumber: "0",
     },
-  ],
+  ] as any[],
 });
+
+let addUser = () => {
+  addUserDialogVisible.value = true;
+};
+
+let submitAddUser = async () => {
+  const sendData = {
+    ...addUserForm.value,
+    waterClassification: JSON.stringify(addUserForm.value.waterClassification),
+    jfyf: dayjs().format("YYYY-MM"),
+  };
+  console.log(addUserForm.value);
+  try {
+    const res = await fetch("http://192.168.88.109:7001/user/create_user", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(sendData),
+    });
+    const data = await res.json();
+    console.log(data.data);
+  } catch (error) {
+    // @ts-ignore
+    ElMessage.error("新增用户失败");
+  }
+};
 
 onMounted(async () => {
   try {
@@ -278,8 +430,6 @@ const handleEditUser = (userHh: string) => {
 };
 
 const handleClose = (done: () => void) => {
-  console.log(done);
-
   done();
 };
 
@@ -287,6 +437,13 @@ const defaultWaterNumber = 0;
 
 const handleAddWaterClassification = () => {
   editForm.value.waterClassification.push({
+    waterType: "",
+    waterNumber: defaultWaterNumber,
+  });
+};
+
+const handleAddSubmitWaterClassification = () => {
+  addUserForm.value.waterClassification.push({
     waterType: "",
     waterNumber: defaultWaterNumber,
   });
@@ -302,6 +459,10 @@ const handleChangeInfo = () => {
 
 const handleDeleteWaterClassification = (index: number) => {
   editForm.value.waterClassification.splice(index, 1);
+};
+
+const handleDeleteSubmitWaterClassification = (index: number) => {
+  addUserForm.value.waterClassification.splice(index, 1);
 };
 </script>
 
