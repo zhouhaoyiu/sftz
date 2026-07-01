88506f1111f7fda5c36ed1b19c102c0013dfa3f3
Radwimps <67226385+zhouhaoyiu@users.noreply.github.com>
2026-07-01 19:54:21 +0800

    chore: remove vulnerable unused dev dependencies

--- src/views/excelInput.vue
+++ src/views/excelInput.vue
@@ -13,23 +13,18 @@
 
 <script lang="ts" setup>
 import { ref } from "vue";
-import * as XLSX from "xlsx";
+import readXlsxFile from "read-excel-file/browser";
 let excelInfo = ref<any>(null);
-const importExcel = (e: any) => {
+const importExcel = async (e: any) => {
   const file = e.target.files[0];
-  const reader = new FileReader();
-  reader.onload = (e: any) => {
-    const data = e.target.result;
-    const workbook = XLSX.read(data, {
-      type: "binary",
-    });
-    const sheetNames = workbook.SheetNames;
-    const worksheet = workbook.Sheets[sheetNames[0]];
-    const json = XLSX.utils.sheet_to_json(worksheet);
-    excelInfo.value = json;
-    console.log(json);
-  };
-  reader.readAsBinaryString(file);
+  if (!file) return;
+  const rows = await readXlsxFile(file);
+  const [headers = [], ...body] = rows;
+  const json = body.map((row) =>
+    Object.fromEntries(headers.map((header, index) => [String(header ?? index), row[index]])),
+  );
+  excelInfo.value = json;
+  console.log(json);
 };
 
 const clickFile = () => {
